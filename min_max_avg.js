37a873dbf1b6fb4dd0e9392e3f35f64ca8a265a3
Tanya Filonych <tfilonych@mail.ru>
2014-06-05 15:00:23 +0300

    created application for counting max, min and average of random numbers

---
+++ min_max_avg.js
@@ -0,0 +1,56 @@
+var counter = 0;
+var arrayOfDigits = [];
+
+function addNumber(){
+	counter++;
+	var newNumDiv = document.createElement('div');
+	newNumDiv.setAttribute('class', 'newNum');
+	newNumDiv.setAttribute('id', 'block' + counter);
+	var numberInputField = document.getElementById("someNumber");
+	var inputFieldText = numberInputField.value;
+	var digitTextNode = document.createTextNode(inputFieldText);
+	newNumDiv.appendChild(digitTextNode);
+	var wrapper = document.getElementById("wrap");
+	wrapper.appendChild(newNumDiv);
+	var digit = parseInt(inputFieldText);
+	arrayOfDigits.push(digit);
+	
+	var max = arrayOfDigits[0];
+	for(i=1; i<arrayOfDigits.length; i++){
+		if(max<arrayOfDigits[i])
+			max=arrayOfDigits[i];
+	}
+	var maxNumberDiv = document.getElementById("maxNum");
+	var textNodeOfmaxNumberDiv = maxNumberDiv.firstChild;
+	textNodeOfmaxNumberDiv.nodeValue = max;
+	
+	var min = arrayOfDigits[0];
+	for(i=1; i<arrayOfDigits.length; i++){
+		if(min>arrayOfDigits[i])
+			min = arrayOfDigits[i];
+	}
+	var minNumberDiv = document.getElementById("minNum");
+	var textNodeOfminNumberDiv = minNumberDiv.firstChild;
+	textNodeOfminNumberDiv.nodeValue = min;
+	
+	var sum = 0;
+	for(i=0; i<arrayOfDigits.length; i++){
+	sum = sum + arrayOfDigits[i];
+	average = sum / arrayOfDigits.length;
+	if (average%1 !== 0){
+		var average = average.toFixed(1);	
+		}	
+		}
+	var avgNumberDiv = document.getElementById("avg");
+	var textNodeOfAvgNumberDiv = avgNumberDiv.firstChild;
+	textNodeOfAvgNumberDiv.nodeValue = average;
+	
+	someNumber.value = "";
+}
+
+function pressEnter(evt){
+	var charCode = (typeof evt.which === "number") ? evt.which : evt.keyCode;
+	if (charCode == 13){
+		addNumber();
+	} 
+}
