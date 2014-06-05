eaf32ca314055e9e39d64575be4b7c3ed4403294
Tanya Filonych <tfilonych@mail.ru>
2014-06-05 16:46:33 +0300

    made formatting of js code

--- min_max_avg.js
+++ min_max_avg.js
@@ -1,7 +1,7 @@
 var counter = 0;
 var arrayOfDigits = [];
 
-function addNumber(){
+function addNumber() {
 	counter++;
 	var newNumDiv = document.createElement('div');
 	newNumDiv.setAttribute('class', 'newNum');
@@ -19,35 +19,45 @@ function addNumber(){
 	var min = arrayOfDigits[0];
 	var average;
 	var sum = 0;
-	for(i=0; i<arrayOfDigits.length; i++){
-		if(max<arrayOfDigits[i]){
+	
+	for (i=0; i<arrayOfDigits.length; i++) {
+		
+		if (max<arrayOfDigits[i]) {
 			max=arrayOfDigits[i];
 		}
+		
 	var maxNumberDiv = document.getElementById("maxNum");
 	var textNodeOfmaxNumberDiv = maxNumberDiv.firstChild;
 	textNodeOfmaxNumberDiv.nodeValue = max;
-		if(min>arrayOfDigits[i]){
+		
+		if (min>arrayOfDigits[i]) {
 			min = arrayOfDigits[i];
 		}
+		
 	var minNumberDiv = document.getElementById("minNum");
 	var textNodeOfminNumberDiv = minNumberDiv.firstChild;
 	textNodeOfminNumberDiv.nodeValue = min;
+	
 	sum = sum + arrayOfDigits[i];
 	average = sum / arrayOfDigits.length;
-	if (average%1 !== 0){
-		var average = average.toFixed(1);	
-	}
+	
+		if (average%1 !== 0) {
+			average = average.toFixed(1);	
+		}
+		
 	var avgNumberDiv = document.getElementById("avg");
 	var textNodeOfAvgNumberDiv = avgNumberDiv.firstChild;
 	textNodeOfAvgNumberDiv.nodeValue = average;
-		}	
+	
+	}	
 
 	someNumber.value = "";
 }
 
-function pressEnter(evt){
+function pressEnter(evt) {
 	var charCode = (typeof evt.which === "number") ? evt.which : evt.keyCode;
-	if (charCode == 13){
+	
+	if (charCode == 13) {
 		addNumber();
 	} 
 }
